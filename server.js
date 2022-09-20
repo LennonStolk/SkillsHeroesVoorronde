218f9589773c9f64e200135a4d885f2f16f843fd
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-09-20 13:07:49 +0200

    Project set up: SQLite

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var app = express();
 const PORT = 8080;
+const sqlite3 = require("sqlite3");
 
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 
 // Index page
 app.get("/", function(req, res) {
-  res.render("Pages/GameList");
+  let db = new sqlite3.Database('./spellenwinkel.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    db.all(`SELECT * FROM games`, [], (err, rows) => {
+      res.render("Pages/GameList");
+    });
+  });
 });
 
 // Run server
