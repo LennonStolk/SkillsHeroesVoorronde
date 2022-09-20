5cc2d12354efe6bdaa3dc3de7de8e7f39d1dc73d
LennonStolk <70744566+LennonStolk@users.noreply.github.com>
2022-09-20 11:58:32 +0200

    Project setup: Gebruikt nu Bootstrap CDN in plaats van NPM package

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var express = require("express");
 var app = express();
 const PORT = 8080;
 
-// Includes bootstrap
-app.use('/css', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/js')))
-
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 
